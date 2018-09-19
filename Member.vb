cff2d870089a8e4f5cd71f6f7b3a7440560be63e
WillemMilton <42041347+WillemMilton@users.noreply.github.com>
2018-09-19 16:39:46 +0200

    Member Code

---
+++ Member.vb
@@ -0,0 +1,20 @@
+﻿' *********************************************************************************
+' TEAM NUMBER: 13
+' Member 1: MONCHO, R.M (216028656)
+' Member 2: SEKGOTO, N.L (218031731)
+' Member 3: NTOAMPE, L.H (201314444)
+' Member 4: MJEKULA, C (218076052)
+' Class name: 
+' *********************************************************************************
+Option Strict On
+Option Explicit On
+Option Infer Off
+Public Class Member
+    Private _Surname As String
+    Private _Name As String
+    Private _Age As Integer
+    Private _Birthday As String
+    Private _Adress As String
+
+
+End Class
